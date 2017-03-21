08c58bbd47e8bd118b863a8655edba2b21b1f991
Alice Jinglu Lu <jlu2@loyalty.com>
2017-03-21 08:29:00 -0400

    added more code

--- notebooks/Users/ALu2@loyalty.com/Alice_test.py
+++ notebooks/Users/ALu2@loyalty.com/Alice_test.py
@@ -3,6 +3,12 @@ from pyspark import sparkContext
 
 sc = SparkContext()
 
+sc = SparkContext("local", "simple App")
+logData = sc.textFile(logFile).cache()
+
+numAs = logData.filter(lambda s: 'a' in s).count()
+
+
 
 
 
